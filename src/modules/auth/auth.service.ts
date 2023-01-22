9b3885e411d4f930694eb6077fef1bac04262ca4
Vyacheslav Shcherbakov <slavacssworld@gmail.com>
2023-01-23 02:04:19 +0500

    bug fix

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -22,7 +22,7 @@ export class AuthService {
 
     }
 
-    async loginUser(dto: UserLoginDTO):Promise<any> {
+    async loginUser(dto: UserLoginDTO):Promise<AuthUserResponse> {
         const existUser = await this.userService.findUserByEmail(dto.email)
         if (!existUser){ throw new BadRequestException(AppError.USER_NOT_EXIST) }
         const validatePassword = await bcrypt.compare(dto.password,existUser.password)
